c0b6997920f88ed43df7ef115e9ece32a793358b
Caleb Eby <caleb.eby01@gmail.com>
2017-08-07 16:57:50 -0700

    Reformat build.js file

--- build.js
+++ build.js
@@ -4,18 +4,16 @@ const fetch = require('node-fetch')
 const fs = require('mz/fs')
 const {map} = require('objectfn')
 
-const mdnProperties = 'https://raw.githubusercontent.com/mdn/data/master/css/properties.json'
-const mdnSyntaxes = 'https://raw.githubusercontent.com/mdn/data/master/css/syntaxes.json'
+const mdnProperties =
+  'https://raw.githubusercontent.com/mdn/data/master/css/properties.json'
+const mdnSyntaxes =
+  'https://raw.githubusercontent.com/mdn/data/master/css/syntaxes.json'
 
 const getJSON = url => fetch(url).then(d => d.json())
 const getSyntax = obj => map(obj, v => v.syntax)
 const writeJSON = path => data => fs.writeFile(path, JSON.stringify(data))
+const createSyntaxFile = (url, file) =>
+  getJSON(url).then(getSyntax).then(writeJSON(file))
 
-getJSON(mdnProperties)
-  .then(getSyntax)
-  .then(writeJSON('properties.json'))
-
-getJSON(mdnSyntaxes)
-  .then(getSyntax)
-  .then(writeJSON('syntaxes.json'))
-
+createSyntaxFile(mdnProperties, 'properties.json')
+createSyntaxFile(mdnSyntaxes, 'syntaxes.json')
